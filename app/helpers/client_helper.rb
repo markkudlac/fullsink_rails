b89df74b661248101522bcc518b137656ae35fc4
Mark Kudlac <mark.kudlac@sympatico.ca>
2013-07-23 12:03:27 -0400

    first push of new site

--- app/helpers/client_helper.rb
+++ app/helpers/client_helper.rb
@@ -1,2 +1,13 @@
 module ClientHelper
+  
+  def geturl
+    
+    if Rails.env.production? then
+      url =  "http://www.fullsink.com"
+    else 
+      url = "http://192.168.1.103:3000"
+    end
+    return url
+    
+  end
 end
